f5cf5e861eb77f5181bf43b22267776081550173
Magomedov Gazi-Magomed <85029421+skyyyylark@users.noreply.github.com>
2021-10-30 16:03:48 +0600

    Add files via upload

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
+import datetime
+import random
+
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework import status
@@ -5,9 +8,11 @@ from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .serializers import MovieListSerializer, ProductsSerialzer, PrReviewsSerializer, PrTagSerializer, \
     CreateMovieValidateSerializer, CreateProductValidateSerializer
-from main.models import Movie, Product, Category, Tag, Review, PrReviews, PrTag
+from main.models import Movie, Product, Category, Tag, Review, PrReviews, PrTag, ConfirmCode
 from django.core.mail import send_mail
 
 
@@ -147,3 +152,34 @@ def register(request):
         return Response(data={
             'message':'User created'
         })
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+        user = User.objects.create_user(
+            username=username,
+            email='a@n.ru',
+            password=password,
+            is_active=False
+        )
+        code = str(random.randint(1000, 9999))
+        valid_until = datetime.datetime.now() + datetime.timedelta(minutes=20)
+        ConfirmCode.objects.create(user=user, code=code, valid_until=valid_until)
+        send_mail('code', 'Here is your registration code', 'gmmagomedov04@gmail.com',
+                  ['gmmagomedov04@gmail.com'], fail_silently=False)
+        return Response(data={'message': 'User created'})
+
+class ConfirmAPIView(APIView):
+    def post(self, request):
+        code = request.data['code']
+        code_list = ConfirmCode.objects.filter(code=code,
+                                               valid_until__gte=datetime.datetime.now())
+        if code_list:
+            user = code_list[0]
+            user.is_active = True
+            user.save()
+            code_list.delete()
+            return Response(data={'message': 'User activated =)'})
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
